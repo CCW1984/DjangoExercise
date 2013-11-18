97c1df6378b49023c9c45d94760f879065069f1d
CCW1984 <christopher.whittemore@gmail.com>
2013-11-17 19:04:51 -0500

    Part 5 Done
    Tutorial Part 5 Done

--- VoyagesTest/polls/urls.py
+++ VoyagesTest/polls/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
 from polls import views
 
-admin.autodiscover()
-
-urlpatterns = patterns('',
+urlpatterns = patterns ('',
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
